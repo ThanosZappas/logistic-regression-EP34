f599aeba0e39dde4e50588378977b9f197e2a509
ThanosZappas <it2021030hua.gr>
2024-11-07 14:23:37 +0200

    changed centers to [0,8] [0,8]

--- experiment1.py
+++ experiment1.py
@@ -4,18 +4,23 @@ from generate_dataset import generate_binary_problem
 from logistic_regression import LogisticRegressionEP34
 
 # Generate dataset
-X, y = generate_binary_problem(centers=np.array([(0,4),(0,4)]), N=1000)
+centers = np.array([(0,8),(0,8)])
+X, y = generate_binary_problem(centers, N=1000)
 
 # Split dataset into training (70%) and testing (30%) sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.70, random_state=50)
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.70)
 
 # Initialize the LogisticRegressionEP34 model
 model = LogisticRegressionEP34()
 
 # Train the model
+print("For dataset with centers: ")
+print(centers)
+print()
 model.fit(X_train, y_train, show_line=True, batch_size=None, iterations=10000)
 
 # Calculate accuracy on the test set
 predictions = model.predict(X_test) >= 0.5
 accuracy = np.mean(predictions == y_test)
+print()
 print(f"Model accuracy: {accuracy * 100:.2f}%")
